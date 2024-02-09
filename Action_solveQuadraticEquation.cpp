6a57ed15c35e4d409c082164dcc5584e614c4f1f
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-09 12:44:09 +0300

    Добавьте файлы проекта.

---
+++ Action_solveQuadraticEquation.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void solveQuadraticEquation(double a, double b, double c) {
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        double root1 = (-b + sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Два корня: " << root1 << " и " << root2 << endl;
+    }
+    else if (discriminant == 0) {
+        double root = -b / (2 * a);
+        cout << "Один  корень: " << root << endl;
+    }
+    else {
+        cout << "Нету корней" << endl;
+    }
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "");
+    double a, b, c;
+    cout << "Введите коэффициенты (a, b, c) квадратного уравнения: ";
+    cin >> a >> b >> c;
+
+    solveQuadraticEquation(a, b, c);
+
+    return 0;
+}
